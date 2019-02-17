9995375c737f69088f0faed1a10282eb351aa85f
Jeremy Tsukasa Zimmerman <jtzimmerm@gmail.com>
2019-02-17 15:19:21 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,29 @@
-//console.log("Up and running!");
+console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
+//
+// // console.log("User flipped " + cardOne);
+// // console.log("User flipped " + cardFour);
 
+var cards = ["queen", "queen", "king", "King"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+var cardTwo = cards[2];
+console.log("User flipped " + cardTwo);
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1])  {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
